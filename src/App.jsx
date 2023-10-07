671fbc8d5b04b5b1359eceb9901ba5f58d8dda7f
Haafii <hafismuhammedtk@gmail.com>
2023-10-07 11:02:56 +0530

    Catagories rout added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Products from './pages/Products';
 import Cart from './pages/Cart';
+import Catagories from './pages/Catagories';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="home" element={<Home />} />
           <Route path="products" element={<Products />} />
           <Route path="cart" element={<Cart />} />
+          <Route path="catagories" element={<Catagories />} />
         </Routes>
       </Router>
     </div>
